d7e4bd533c02f37a1eb2490b4a41d1f4ea9c9bca
Kiril207 <kirya.sherstyuk.05@mail.ru>
2025-06-04 05:19:23 +0000

    cmake cg

--- SFML-2.5.1/build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/src/SFML/System/Clock.cpp \
- /usr/include/stdc-predef.h \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Clock.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Export.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/Config.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Time.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/src/SFML/System/Unix/ClockImpl.hpp
--- SFML-2.5.1/build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o: \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/src/SFML/System/Lock.cpp \
- /usr/include/stdc-predef.h \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Lock.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Export.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/Config.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/NonCopyable.hpp \
- /home/Kirill/Kiril207/workspace/projects/16-Games/04/SFML-2.5.1/include/SFML/System/Mutex.hpp
